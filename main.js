95ae2fb3fba42c166f120b09efe14076f39a8135
Agastyasud <72261120+Agastyasud@users.noreply.github.com>
2021-05-05 17:09:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+canvas1=document.getElementById("Maincanvas");
+asd=canvas1.getContext("2d")
+asd.beginPath(); asd.strokeStyle = "grey"; asd.lineWidth = 1; asd.rect(150, 143, 430, 200); asd.stroke();
+asd.beginPath(); asd.strokeStyle = "blue"; asd.lineWidth = 5; asd.arc(250, 210, 40 ,0 , 2 * Math.PI); asd.stroke()
+asd.beginPath(); asd.strokeStyle = "black"; asd.lineWidth = 5; asd.arc(350, 210, 40 ,0 , 2 * Math.PI); asd.stroke()
+asd.beginPath(); asd.strokeStyle = "red"; asd.lineWidth = 5; asd.arc(450, 210, 40 ,0 , 2 * Math.PI); asd.stroke();
+asd.beginPath(); asd.strokeStyle = "orange"; asd.lineWidth = 5; asd.arc(300, 250, 40 ,0 , 2 * Math.PI); asd.stroke();
+asd.beginPath(); asd.strokeStyle = "green"; asd.lineWidth = 5; asd.arc(400, 250, 40 ,0 , 2 * Math.PI); asd.stroke();
